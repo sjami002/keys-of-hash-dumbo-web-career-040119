26281c6f252d3604fd4b441a7be70135c2fa000e
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-20 02:40:47 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys = collect do |key, val|
+      if arguments.include?val
+         key
+      end
+    end
+    keys.compact
   end
 end
